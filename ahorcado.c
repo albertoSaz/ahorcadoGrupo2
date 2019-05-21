1b827cf53582a92944f38e598480aa814464c22f
MiquelEscala <49230218+MiquelEscala@users.noreply.github.com>
2019-05-21 12:21:02 +0200

    Optimizando y comentando el codigo

--- ahorcado.c
+++ ahorcado.c
@@ -4,8 +4,8 @@
 
 int main() {
 char frase[60],rep[100],temporal[100];
-char pal=-87;
-int longitud=0,i,j=0,inicial=0,acertado=0,temp=0,oportunidades=5;
+char pal;
+int longitud=0,i,j=0,inicial=0,acertado=0,correct=0,oportunidades=5;
 int repetido=0,gano=0;
 
 printf("\tJuego del Ahorcado\n");
@@ -17,88 +17,103 @@ system("cls");
 
 do {
 
-  temp=0;
+  correct=0;
 
+//Inicia el programa con las barrabajas correspondientes a la palabra
   if(inicial == 0) {
     for(i=0;i<strlen(frase);i++) {
+
       if(frase[i] == ' ') {
         temporal[i] = ' ';
         longitud++;
       }
+
       else {
         temporal[i] = '_';
         longitud++;
       }
     }
-  inicial = 1;
-  temporal[longitud] = '\0';
-  }
-  else {
 
-    for(i=0;i<strlen(rep);i++) {
-      if(rep[i] == pal) {
-        repetido = 1;
-        break;
-      }
-      else {
-        repetido = 0;
-      }
-    }
-
-    if(repetido == 0) {
-      rep[j] = pal;
-      j++;
-      for(i=0;i<strlen(frase);i++) {
-        if(frase[i] == pal) {
-          temporal[i] = pal;
-          acertado++;
-          temp=1;
-        }
-      }
-    }
-
-    if(repetido == 0) {
-      if(temp == 0 && pal!=-87) {
-      oportunidades = oportunidades - 1;
-      }
-
-      system("cls");
-
-      for(i=0;i<strlen(temporal);i++) {
-        printf(" %c ",temporal[i]);
-      }
-
-      printf("\n");
-
-      if(strcmp(frase,temporal) == 0) {
-        gano = 1;
-        break;
-      }
-      printf("\n");
-      printf("Letras Acertadas: %d",acertado);
-      printf("\n");
-      printf("Oportunidades Restantes: %d",oportunidades);
-      printf("\n");
-    }
-    else {
-      printf("Ya se ha introducido la '%c' en esta partida. ",pal);
-    }
-    if (oportunidades!=0)
-    {
-      printf("Introduzca una letra:");
-      scanf(" %c",&pal);
-    }
+    inicial = 1;
+    correct=1;
+    temporal[longitud] = '\0';
 
   }
+  //Compara las letras introducidas anteriormente con la actual
+  for(i=0;i<strlen(rep);i++) {
+    if(rep[i] == pal) {
+      repetido = 1;
+      break;
+
+    }else {
+      repetido = 0;
+    }
+  }
+  //Guarda la letra introducida si no esta repetida
+  if(repetido == 0 && correct==0) {
+    rep[j] = pal;
+    j++;
+
+    //Recorre la palabra para encontrar coincidencias
+    for(i=0;i<strlen(frase);i++) {
+
+      if(frase[i] == pal) {
+        temporal[i] = pal;
+        acertado++;
+        correct=1;
+      }
+
+    }
+  }
+
+
+    //Resta por error
+    if(correct == 0) {
+    oportunidades = oportunidades - 1;
+    }
+
+    system("cls");
+    //Imprime la palabra con las ketras correctas introducidas anteriormente y barras para las que no
+    for(i=0;i<strlen(temporal);i++) {
+      printf(" %c ",temporal[i]);
+    }
+
+    printf("\n");
+    //Si temporal coincide con la palabbra original sale del bucle indicando que se ha ganado el juego
+    if(strcmp(frase,temporal) == 0) {
+      gano = 1;
+      break;
+    }
+
+    if(repetido == 1) {
+      printf("\nYa se ha introducido la '%c' en esta partida. ",pal);
+    }
+
+    //Escribe nuestras estadisticas de partida a cada ronda
+    printf("\n");
+    printf("Letras Acertadas: %d",acertado);
+    printf("\n");
+    printf("Oportunidades Restantes: %d",oportunidades);
+    printf("\n");
+
+
+  //Recoge letras mientras aun queden oportunidades
+  if (oportunidades!=0){
+    printf("Introduzca una letra:");
+    scanf(" %c",&pal);
+  }
+
+
 
 }while(oportunidades != 0);
 
 
 if(gano) {
+
   printf("\n\n");
   printf("Enhorabuena, has ganado :)");
-}
-else {
+
+}else {
   printf("\n\n");
   printf("Has perdido.\n");
   printf("\t\t\t__\n\t\t\t| |\n\t\t\t| 0\n\t\t\t|/|\\ \n\t\t\t|/ \\ ");
